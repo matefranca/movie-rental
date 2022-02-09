16a151466bafb0c082180f6451b146b5a8d4e1fb
Matheus Franca <matheus363@hotmail.com>
2022-02-09 00:37:25 -0300

    BackEnd

---
+++ BackEnd/BackEnd/Models/Rental.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Rental
+    {
+        public int ID { get; set; }
+        public int ID_Cliente { get; set; }
+        public int ID_Filme { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public DateTime DataDevolucao { get; set; }
+    }
+}
